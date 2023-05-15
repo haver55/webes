c81fc80090d95e3b8a104cf552f174e4eb347171
haver55 <100092228+haver55@users.noreply.github.com>
2023-05-15 12:47:16 +0200

    Add files via upload

---
+++ projekt2/alap.php
@@ -0,0 +1,125 @@
+<?php
+if(isset($_GET["redirect"]) && isset($_GET["delay"]))
+{
+    header("Refresh: ".$_GET["delay"]."; url=https://".$_GET["redirect"]);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <style>
+
+        body{
+            background-color: black;
+            font-family: Arial;
+            font-size: 20px;
+            margin: 0;
+        }
+
+        #everyting{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        #info-container{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 20%;
+            width: 100%;
+            background-color: #000000;
+            color: #ffffff;
+            font-family: Arial;
+            font-size: 20px;
+        }
+
+        #response-contaienr{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 20%;
+            width: 100%;
+            background-color: <?php if(isset($_GET["color"])){echo $_GET["color"];} else {echo "black";}  ?>;
+            color:  #00ffd5;
+            font-family: Arial;
+            font-size: 20px;
+        }
+
+        #login-container{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 60%;
+            width: 100%;
+            background-color: #000000;
+            color: #ffffff;
+            font-family: Arial;
+            font-size: 20px;
+        }
+
+        input[type=text], input[type=password] {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            box-sizing: border-box;
+            border: 2px solid #000000;
+            border-radius: 4px;
+        }
+
+        input[type=submit] {
+            width: 100%;
+            background-color: #000000;
+            color: #ffffff;
+            padding: 14px 20px;
+            margin: 8px 0;
+            border: 2px solid #ffffff;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type=submit]:hover {
+            background-color: #ffffff;
+            color: #000000;
+        }
+
+        form{
+            width: 50%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+    </style>
+<head>
+    <title>Login</title>
+</head>
+<body >
+    <div id= "everyting">
+        <div id="info-container" >
+                <p>Nagy Ákos<br>I155A6</p>
+        </div>
+        <div id= "response-contaienr">
+            <?php
+                if(isset($_GET["response"]))
+                {
+                    echo $_GET["response"];
+                }
+            ?>
+        </div>
+        <div id="login-container" >
+            <form action="login.php" method="post">
+                <input type="text" name="user" placeholder="Email"><br>
+                <input type="password" name="password" placeholder="Password"><br>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ projekt2/login.php
@@ -0,0 +1,93 @@
+<?php 
+
+function Decode()
+{
+    $content = file_get_contents("password.txt");
+    $offsets = array(5,-14,31,-9,3);
+
+    $result = "";
+
+    foreach(explode("\n", $content) as $line)
+    {
+        if (strlen($line) == 0)
+            continue;
+
+        for($i = 0; $i < strlen($line); $i++)
+        {
+            $result .= chr(ord($line[$i]) - $offsets[$i % count($offsets)]);
+        }
+        $result .= "\n";
+    }   
+
+    return $result;
+}
+
+
+function Get_Color($LogInUser)
+{
+
+    $sname= "127.0.0.1";
+    $uname= "root";
+    $password = "";
+    $db_name = "adatbazisom";
+
+    $conn = mysqli_connect($sname, $uname, $password, $db_name);
+    if (!$conn)
+    {
+        echo "Connection failed!";
+    }
+
+    $sql = "SELECT titkos FROM `project2` WHERE username='$LogInUser'";
+
+    $color = mysqli_query($conn, $sql);
+
+    $color = mysqli_fetch_assoc($color);
+
+    $color = $color['titkos'];
+
+    $colors =array("piros" => "red", "zold" => "green","sarga" => "yellow", "kek" => "blue",  "fekete" => "black", "feher" => "white");
+
+    $color = $colors[$color];
+
+    return $color;
+}
+
+
+$LogInUser = $_POST['user'];
+$LogInPassword = $_POST['password'];
+
+$passwords = Decode();
+
+$validUser = false;
+$correctPassword = false;
+
+foreach(explode("\n", $passwords) as $line)
+{
+    if (strlen($line) == 0)
+        continue;
+
+    $line = explode("*", $line);
+
+    if ($line[0] == $LogInUser)
+    {
+        $validUser = true;
+        if ($line[1] == $LogInPassword)
+        {
+            $correctPassword = true;
+            break;
+        }
+    }
+}
+
+if ($validUser == false){
+    header("Location: index.php?response=Invalid User!");
+}
+else if ($correctPassword == false){
+    header("Location: index.php?response=Invalid password!&redirect=police.hu&delay=3");
+
+}else{
+    $color = Get_Color($LogInUser);
+    header("Location: index.php?response=Welcome!&color=$color");
+}
+
+?>
